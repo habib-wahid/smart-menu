830cdbe077de5eb84d6424db4576450821b276fb
habib-wahid <habib11wahid@gmail.com>
2025-06-03 14:11:55 +0600

    check workflow

--- menu-app/src/main/java/org/example/menuapp/MenuAppApplication.java
+++ menu-app/src/main/java/org/example/menuapp/MenuAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MenuAppApplication{
 
     public static void main(String[] args) {
+        System.out.println("Check workflow");
         SpringApplication.run(MenuAppApplication.class, args);
     }
 
