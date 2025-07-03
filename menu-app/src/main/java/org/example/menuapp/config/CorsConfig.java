fe13b975825b6202a79d8498900851c1cf0cba76
habib-wahid <habib11wahid@gmail.com>
2025-07-03 11:41:25 +0600

    cors added for admin

--- menu-app/src/main/java/org/example/menuapp/config/CorsConfig.java
+++ menu-app/src/main/java/org/example/menuapp/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
